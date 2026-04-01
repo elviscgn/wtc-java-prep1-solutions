33025cc44a6c0c378081655b72344ed26576e8e9
Elvis Chege <96030189+elviscgn@users.noreply.github.com>
2026-04-01 07:47:01 +0200

    shapes done

--- solutions/ex04_polymorphism/Shape.java
+++ solutions/ex04_polymorphism/Shape.java
@@ -3,6 +3,9 @@ package ex04_polymorphism;
 public class Shape {
     protected String color;
 
+    public Shape(String color) {
+        this.color = color;
+    }
 
     public double calculateArea() {
         return 0;
